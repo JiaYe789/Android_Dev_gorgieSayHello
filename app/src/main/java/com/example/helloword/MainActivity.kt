b61b40517e3976ad1e85832efdd6237c75d009ac
Jia Ye <jiaye@Jias-MacBook-Air.local>
2022-08-27 22:58:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.Hello_button)
+        button.setOnClickListener {
+            lag.v("Hello World", "Button Clicked!")
+        Toast.makeText(this, "Hello to you too!", Toast.LENGTH.SHORT).show()
+        }
+
+    }
+
+}
